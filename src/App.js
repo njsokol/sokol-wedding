d5f8a23f13c236bbf41c6e3d628f2f1e7bcb551b
njsokol <njsokol@us.ibm.com>
2018-10-14 22:26:19 -0500

    feat: get all the content in there

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div style={{ padding: "3rem 0" }}>
+          <h1 className="text-3">Saturday, January 26, 2019</h1>
+        </div>
+        <h3>Time of the Event</h3>
+        <h3>Location</h3>
+        <h3>Link to Timeline</h3>
+        <h3>Duration of the Event</h3>
+        <p>Ceremony - 20 Minutes</p>
+        <p>Cocktail Hour- 1 Hour</p>
+        <p>Reception- </p>
+
+        <p>How to get to the ceremony/ hotel room block</p>
+        <p>Hotel room block reservation</p>
+
+        <p>Are you from out of town? (Tour Guide Page) </p>
+        <p>
+          Suggestions on what to do on Friday night (Restaurant suggestions
+          around the hotel) and what to do on Saturday day before the wedding…
+          In Austin or around the area
+        </p>
+        <p>
+          Suggestions for dinner: The Oasis, Moviehouse and Eatery, Jennas Asian
+          Kitchen
+        </p>
+        <p>Go Into Austin: Chuy’s, For Tex Mex</p>
+
+        <p>Attire- Casual</p>
+
+        <p>Registry- </p>
+        <p>Pottery Barn</p>
+        <p>Crate and Barrel</p>
+        <p>Bed Bath and Beyond</p>
       </div>
     );
   }
