83db7bee819c767e6440e7f241a4f9cb33e8843a
njsokol <njsokol@us.ibm.com>
2018-10-20 13:24:54 -0500

    feat: add in engagement photos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import React, { Component } from "react";
 import "./App.css";
+import Image1 from "./images/engagement-photos/AW0A1166bw.jpg";
+import Image2 from "./images/engagement-photos/AW0A1079.JPG";
+import Image3 from "./images/engagement-photos/AW0A1117.JPG";
+import Image4 from "./images/engagement-photos/AW0A1191.JPG";
+import Image5 from "./images/engagement-photos/AW0A1284.JPG";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div style={{ padding: "3rem 0" }}>
-          <h1 className="text-3">Saturday, January 26, 2019</h1>
+        <div className="banner" style={{ backgroundImage: `url(${Image1})` }}>
+          <h1 className="banner-text">McKenzie & Nathan</h1>
         </div>
-        <h3>Time of the Event</h3>
-        <h3>Location</h3>
-        <h3>Link to Timeline</h3>
-        <h3>Duration of the Event</h3>
-        <p>Ceremony - 20 Minutes</p>
-        <p>Cocktail Hour- 1 Hour</p>
-        <p>Reception- </p>
+        <div style={{ padding: "3rem 0" }}>
+          <h1 className="heading-1">When:</h1>
+          <h1 className="text-2" style={{ textAlign: "center" }}>
+            Saturday, January 26, 2019
+          </h1>
+        </div>
+        <div className="pic-grid">
+          <div
+            className="pic-grid-item"
+            style={{ backgroundImage: `url(${Image2})` }}
+          />
+          <div
+            className="pic-grid-item"
+            style={{ backgroundImage: `url(${Image3})` }}
+          />
+        </div>
+        <div style={{ padding: "3rem 0" }}>
+          <h1 className="heading-1">Location:</h1>
+          <h1 className="text-2" style={{ textAlign: "center" }}>
+            Vintage Villas, Austin, TX
+          </h1>
+        </div>
+        <div className="pic-grid">
+          <div
+            className="pic-grid-item"
+            style={{ backgroundImage: `url(${Image4})` }}
+          />
+          <div
+            className="pic-grid-item"
+            style={{ backgroundImage: `url(${Image5})` }}
+          />
+        </div>
 
-        <p>How to get to the ceremony/ hotel room block</p>
+        {/* <p>How to get to the ceremony/ hotel room block</p>
         <p>Hotel room block reservation</p>
 
         <p>Are you from out of town? (Tour Guide Page) </p>
@@ -36,7 +66,7 @@ class App extends Component {
         <p>Registry- </p>
         <p>Pottery Barn</p>
         <p>Crate and Barrel</p>
-        <p>Bed Bath and Beyond</p>
+        <p>Bed Bath and Beyond</p> */}
       </div>
     );
   }
